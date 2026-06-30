93b734e4269afe8f7e94fe3b9ac2e123597e5306
Memo <w407495@gmail.com>
2026-06-30 23:47:20 +0530

    Added persistence version of Todo API

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
+const fs =require("fs");
+const path = require("path");
 const port = 3000;
+const filename = path.join(__dirname,"todo.json")
+function loadTodo(){
+    if(!fs.existsSync(filename)){
+        fs.writeFileSync(filename,JSON.stringify([],null,2));
+    }
+    const dos= fs.readFileSync(filename);
+    return JSON.parse(dos);
+}
+let todos = loadTodo();
+function saveTodo(){
+fs.writeFileSync(filename,JSON.stringify(todos,null,2));
+}
 
 app.use(express.json());
-let todos = [
-    {id:1,task:"Learning Basics of express Framework"},
-    {id:2,task:"Project one- To Do list Api"},
-    {id:3,task:"I did CRUD operations in todo Api"},
-    {id:4,task:"Will be added soon"}
-];
-
 app.get('/',(req,res)=>{
     res.json("I made Todo api. Use endpoint todos to get them.😎😊");
 });
@@ -25,9 +32,10 @@ if(!todo){return res.status(404).json(" todo not found.")}
 res.json(todo);
 })
 app.post('/todos',(req,res)=>{
-    if(!req.body.task){res.json(" Task is mandatory. Please write the task.")}
+    if(!req.body.task){res.status(404).json(" Task is mandatory. Please write the task.")}
 const newtodo={id:(todos.length+1),task:req.body.task}
 todos.push(newtodo);
+saveTodo();
 res.status(201).json(newtodo);
 })
 app.put('/todos/:id',(req,res)=>{
@@ -37,13 +45,15 @@ if(!todo){
     res.status(404).json("Id not found.");
 }
 todo.task=req.body.task;
+saveTodo();
 res.json(todo);
 })
 app.delete('/todos/:id',(req,res)=>{
     const id = parseInt(req.params.id);
     const todo = todos.findIndex(t=>t.id ===id);
     if(todo === -1){res.status(404).json("Todo not found.")}
-    const deletedtodo = todos.splice(todo,1);
+    const [deletedtodo] = todos.splice(todo,1);
+    saveTodo();
     res.json(deletedtodo);
 })
 
