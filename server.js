38267e50008f668a47428392021f077d4dbfc6d6
Memo <w407495@gmail.com>
2026-07-04 15:31:15 +0530

    Changed file persistence to Database persistence

--- server.js
+++ server.js
@@ -1,74 +1,79 @@
 const express = require("express");
 const app = express();
-const fs =require("fs");
 const path = require("path");
 const port = 3000;
-const filename = path.join(__dirname,"todo.json")
-function loadTodo(){
-    if(!fs.existsSync(filename)){
-        fs.writeFileSync(filename,JSON.stringify([],null,2));
-    }
-    const dos= fs.readFileSync(filename);
-    return JSON.parse(dos);
-}
-let todos = loadTodo();
-function saveTodo(){
-fs.writeFileSync(filename,JSON.stringify(todos,null,2));
-}
+const mongoose =require("mongoose");
+//connection
+mongoose.connect('mongodb://127.0.0.1:27017/todo-tasks-manager')
+.then(()=> console.log("Mogodb connected."))
+.catch((err)=>console.log("error:"+ err));
+
+/*Using database to handle data*/
+
+const taskSchema = new mongoose.Schema({
+id:{type: Number,
+    unique: true,
+},
+task:{
+    type: String,
+    required: true,
+},
+completed: { type: Boolean, default:false},
+
+
+})
+
+//Creating db model
+const Todo = new mongoose.model("Todo",taskSchema);
 
 app.use(express.json());
 app.get('/',(req,res)=>{
     res.json("I made Todo api. Use endpoint todos to get them.😎😊");
 });
  
-app.get('/todos',(req,res)=>{
+app.get('/todos',async(req,res)=>{
+    const todos =await Todo.find();
     res.json(todos);
+
 });
 
 app.get('/todos/:id',(req,res)=>{
 const id = parseInt(req.params.id);
-const todo = todos.find(t=>t.id===id);
+const todo = Todo.findOne({id : id});
 if(!todo){return res.status(404).json(" todo not found.")}
 res.json(todo);
 })
-//route to mark tasks completed
-app.patch('/todos/:id',(req,res)=>{
-const id = parseInt(req.params.id);
-const todo = todos.find(t=>t.id===id);
-if(!todo){return res.status(404).json(" todo not found.")}
-todo.completed= (!todo).completed;
-res.json(todo);
-})
-app.post('/todos',(req,res)=>{
+app.post('/todos',async(req,res)=>{
     //Validating client's request 
     if(!req.body.task){res.status(404).json(" Task is mandatory. Please write the task.")}
-const newtodo={id:(todos.length+1),task:req.body.task,completed:false};
-todos.push(newtodo);
-saveTodo();
-res.status(201).json(newtodo);
-
+const last = await Todo.findOne().sort({ id: -1 });
+  const nextId = last ? last.id + 1 : 1;
+    const result = await Todo.create({
+    id:nextId,
+    task:req.body.task,
+    completed:false
 })
-
-//
+res.status(201).json(result);
+})
 
 // fitering search by completed Tasks
-app.put('/todos/:id',(req,res)=>{
+app.put('/todos/:id',async(req,res)=>{
 const id = parseInt(req.params.id);
-const todo= todos.find(t=>t.id===id)
+const todo= await Todo.findOneAndUpdate(
+    {id:id},
+    {task:req.body.task},
+)
 if(!todo){
-    res.status(404).json("Id not found.");
+   return res.status(404).json("Id not found.");
 }
-todo.task=req.body.task;
-saveTodo();
 res.json(todo);
 })
+
 app.delete('/todos/:id',(req,res)=>{
     const id = parseInt(req.params.id);
-    const todo = todos.findIndex(t=>t.id ===id);
-    if(todo === -1){res.status(404).json("Todo not found.")}
-    const [deletedtodo] = todos.splice(todo,1);
-    saveTodo();
-    res.json(deletedtodo);
+    const todo = Todo.findOneAndDelete({id : id });
+    if(!todo){res.status(404).json("Todo not found.")}
+    res.json({message:"Todo deleted successfully."});
 })
 
 app.listen(port,()=>{
